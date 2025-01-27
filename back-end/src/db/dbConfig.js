843634d9f11d1bfae2f74a9fb0f2c161b392fe5e
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-01-27 18:16:50 -0300

    fix: conexao com o banco de dados

--- back-end/src/db/dbConfig.js
+++ back-end/src/db/dbConfig.js
@@ -3,11 +3,11 @@ import mysql from 'mysql2/promise';
 export async function openDb() {
   // Substitua pelos seus dados de conexão MySQL
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'gerenciamento_estoque',
-    port: 3307
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    port: process.env.port
   });
   return connection;
 }
