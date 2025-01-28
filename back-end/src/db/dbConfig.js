b28f124b94aa4a63203cbefb1171ab7b16c126cc
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-01-27 21:24:04 -0300

    Update dbConfig.js

--- back-end/src/db/dbConfig.js
+++ back-end/src/db/dbConfig.js
@@ -2,6 +2,12 @@ import mysql from 'mysql2/promise';
 
 export async function openDb() {
   // Substitua pelos seus dados de conexão MySQL
-  const connection = await mysql.createConnection(process.env.MYSQL_URL);
+  const connection = await mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+    port: process.env.port
+  });
   return connection;
 }
