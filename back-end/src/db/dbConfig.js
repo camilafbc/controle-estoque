dc43793cc10b17b5a5b2ff4e6e7a4d032ea26a05
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-01-28 21:59:13 -0300

    teste conexao railway

--- back-end/src/db/dbConfig.js
+++ back-end/src/db/dbConfig.js
@@ -2,12 +2,6 @@ import mysql from 'mysql2/promise';
 
 export async function openDb() {
   // Substitua pelos seus dados de conexão MySQL
-  const connection = await mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
-    port: process.env.port
-  });
+  const connection = await mysql.createConnection(process.env.MYSQL_URL);
   return connection;
 }
