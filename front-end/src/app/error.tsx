ec0a0738dfebc091418ad63b5c2b2d06a34f5486
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-02-02 12:44:36 -0300

    fix: capturando erro para debugs

--- front-end/src/app/error.tsx
+++ front-end/src/app/error.tsx
@@ -2,8 +2,20 @@
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 
-export default function Page() {
+export default function Page({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log do erro no console para análise
+    console.error("Erro capturado:", error);
+  }, [error]);
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-10">
       <div className="relative h-64 w-full max-w-lg">
@@ -17,6 +29,7 @@ export default function Page() {
       </div>
       <div className="flex flex-col items-center justify-center gap-4">
         <h2> Ops! Ocorreu um erro!</h2>
+        <p className="mt-2 text-sm">Detalhes: {error.message}</p>
         <Button
           onClick={() => window.location.reload()}
           className="hover:bg-orange-500/90"
