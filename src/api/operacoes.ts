7563bb500bd4b5d35aa90142d493a93c128a92ab
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-07-17 18:53:50 -0300

    refactor: Corrige a chamada para as novas rotas de operacoes

--- src/api/operacoes.ts
+++ src/api/operacoes.ts
@@ -1,15 +1,20 @@
 import api from "@/lib/axios";
 
-export const getOperacoes = async (id: number) => {
-  const response = await api.get(`/movimentacoes/${id}`);
-  return response.data;
-};
+// export const getOperacoes = async (id: number) => {
+//   const response = await api.get(`/api/user/operacoes/${id}`);
+//   return response.data;
+// };
 
 export const createOperacao = async (operacao: {
   idProduto: number;
   tipoOp: number;
   quantidade: number;
 }) => {
-  const result = await api.post("/movimentacoes/add", operacao);
-  return result;
+  const result = await api.post("/api/user/operacoes", operacao);
+  return result.data;
+};
+
+export const getOperacoesByProduto = async (idProduto: number) => {
+  const response = await api.get(`/api/user/operacoes/produto/${idProduto}`);
+  return response.data;
 };
