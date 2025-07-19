9ffaf96e54c51edf6ea011a4085e78593fa6a2b7
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-07-19 19:46:25 -0300

    refactor(home): Altera chamada de sessão do lado do cliente para o servidor

--- src/components/home/WelcomeUser.tsx
+++ src/components/home/WelcomeUser.tsx
@@ -1,18 +1,18 @@
-"use client";
+import { getServerSession } from "next-auth";
 
-import { useSession } from "next-auth/react";
+import { authOptions } from "@/lib/auth";
 
-export default function WelcomeUser() {
-  const { data: user, status } = useSession();
+export default async function WelcomeUser() {
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="flex items-baseline gap-1 border-b-[1px] border-b-zinc-200">
       <p className="text-lg font-semibold">Olá,</p>
       <span className="text-2xl font-bold tracking-wide">
-        {status === "loading" ? (
+        {!session ? (
           <div className="h-6 w-20 animate-pulse rounded bg-gray-200 dark:bg-gray-700" />
         ) : (
-          user?.user.name?.split(" ")[0] + "!"
+          session.user.name.split(" ")[0] + "!"
         )}
       </span>
     </div>
