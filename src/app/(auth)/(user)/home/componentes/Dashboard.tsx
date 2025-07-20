9e8d4086d4a7ee69fe1a1506bdd207a538335880
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-07-19 21:02:39 -0300

    refactor (dashboard usuario): Transfere as chamadas de consultas a API para o servidor

--- src/app/(auth)/(user)/home/componentes/Dashboard.tsx
+++ src/app/(auth)/(user)/home/componentes/Dashboard.tsx
@@ -1,16 +1,42 @@
-"use client";
-
 import { Boxes, CalendarDays, GraduationCap, Package } from "lucide-react";
+import { getServerSession } from "next-auth";
 
 import DashboardCard from "@/components/dashboard/DashboardCard";
-import { useAuthContext } from "@/context/AuthContext";
-import { useGetCountTurmas } from "@/queries/turmas";
+import { authOptions } from "@/lib/auth";
 
-export default function Dashboard() {
-  const { user } = useAuthContext();
-  const idCurso = Number(user?.curso?.idCurso);
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  const idCurso = session?.user.curso;
 
-  const countTurmas = useGetCountTurmas(idCurso);
+  const [turmasCount, produtosCount, estoqueCount, validadeCount] =
+    await Promise.all([
+      fetch(`${process.env.NEXTAUTH_URL}/api/user/turmas/count/${idCurso}`, {
+        next: {
+          revalidate: 0,
+        },
+      }).then((res) => res.json()),
+      fetch(`${process.env.NEXTAUTH_URL}/api/user/produtos/count/${idCurso}`, {
+        next: {
+          revalidate: 0,
+        },
+      }).then((res) => res.json()),
+      fetch(
+        `${process.env.NEXTAUTH_URL}/api/user/produtos/estoque/${idCurso}`,
+        {
+          next: {
+            revalidate: 0,
+          },
+        },
+      ).then((res) => res.json()),
+      fetch(
+        `${process.env.NEXTAUTH_URL}/api/user/produtos/validade/${idCurso}`,
+        {
+          next: {
+            revalidate: 0,
+          },
+        },
+      ).then((res) => res.json()),
+    ]);
 
   return (
     <div className="grid grid-cols-1 gap-2 md:grid-cols-4">
@@ -18,8 +44,8 @@ export default function Dashboard() {
         delay={0}
         showHeader={false}
         title="Turmas"
-        loading={countTurmas.isFetching}
-        data={String(countTurmas.data)}
+        loading={false}
+        data={turmasCount}
         subtitle="cadastradas e ativas"
         icon={
           <div className="rounded-md bg-navbar p-2 text-white dark:bg-slate-800">
@@ -31,8 +57,8 @@ export default function Dashboard() {
         delay={1}
         showHeader={false}
         title="Produtos"
-        loading={countTurmas.isFetching}
-        data={0}
+        loading={false}
+        data={produtosCount}
         subtitle="cadastrados para esse curso"
         icon={
           <div className="rounded-md bg-navbar p-2 text-white dark:bg-slate-800">
@@ -44,8 +70,8 @@ export default function Dashboard() {
         delay={2}
         showHeader={false}
         title="Validade"
-        loading={countTurmas.isFetching}
-        data={0}
+        loading={false}
+        data={validadeCount}
         subtitle="produtos expirando nos próximos 30 dias"
         icon={
           <div className="rounded-md bg-navbar p-2 text-white dark:bg-slate-800">
@@ -57,8 +83,8 @@ export default function Dashboard() {
         delay={3}
         showHeader={false}
         title="Estoque"
-        loading={countTurmas.isFetching}
-        data={0}
+        loading={false}
+        data={estoqueCount}
         subtitle="produtos em estoque"
         icon={
           <div className="rounded-md bg-navbar p-2 text-white dark:bg-slate-800">
