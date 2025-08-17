b32787903c6bac58992817dcf92499ce59a0d780
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-08-17 19:49:31 -0300

    refactor(menu mobile): Ajusta comportamento para fechar menu após seleção

--- src/components/header/components/MobileMenu.tsx
+++ src/components/header/components/MobileMenu.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
+import { SheetClose } from "@/components/ui/sheet";
 
 interface NavbarProps {
   role?: string;
@@ -13,18 +14,18 @@ export default function MobileMenu({ role }: NavbarProps) {
       <div className="mt-8 flex w-full flex-col p-2">
         <div className="mb-4">
           <h3 className="text-lg font-semibold">Cadastros</h3>
-          <Button
+          <SheetClose
             className="w-full items-center justify-start gap-1 bg-transparent text-blue-senac shadow-none hover:text-blue-950/50"
             asChild
           >
-            <Link href={"/adm/cadastros/cursos"}>Cursos</Link>
-          </Button>
-          <Button
+            <Link href={"/admin/cadastros/cursos"}>Cursos</Link>
+          </SheetClose>
+          <SheetClose
             className="w-full items-center justify-start gap-1 bg-transparent text-blue-senac shadow-none hover:text-blue-950/50"
             asChild
           >
-            <Link href={"/adm/cadastros/usuarios"}>Usuários</Link>
-          </Button>
+            <Link href={"/admin/cadastros/usuarios"}>Usuários</Link>
+          </SheetClose>
         </div>
       </div>
     );
@@ -34,29 +35,29 @@ export default function MobileMenu({ role }: NavbarProps) {
       <div className="mt-8 flex w-full flex-col p-2">
         <div className="mb-4">
           <h3 className="text-lg font-semibold">Cadastros</h3>
-          <Button
+          <SheetClose
             className="w-full items-center justify-start gap-1 bg-transparent text-blue-senac shadow-none hover:text-blue-950/50"
             asChild
           >
             <Link href={"/cadastros/produtos"}>Produtos</Link>
-          </Button>
-          <Button
+          </SheetClose>
+          <SheetClose
             className="w-full items-center justify-start gap-1 bg-transparent text-blue-senac shadow-none hover:text-blue-950/50"
             asChild
           >
             <Link href={"/cadastros/turmas"}>Turmas</Link>
-          </Button>
+          </SheetClose>
         </div>
         <div>
           <h3 className="text-lg font-semibold">Relatórios</h3>
-          <Button
+          <SheetClose
             className="w-full items-center justify-start gap-1 bg-transparent text-blue-senac shadow-none hover:text-blue-950/50"
             asChild
           >
             <Link href={"/relatorios/movimentacao-turma"}>
               Movimentação por Turma
             </Link>
-          </Button>
+          </SheetClose>
         </div>
       </div>
     );
