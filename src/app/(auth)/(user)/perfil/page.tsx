193f47bc10a1bcff3ca8313793322173310b3331
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-08-17 17:47:10 -0300

    refactor(profile): Reorganiza a componentização para edição de dados de perfil

--- src/app/(auth)/(user)/perfil/page.tsx
+++ src/app/(auth)/(user)/perfil/page.tsx
@@ -1,14 +1,13 @@
 import BadgePageTitle from "@/components/BadgePageTitle";
 import MyBreadcrumb from "@/components/MyBreadcrumb";
-
-import Container from "./components/Container";
+import ContainerProfile from "@/components/users/ContainerProfile";
 
 export default function Page() {
   return (
     <div className="space-y-6">
       <MyBreadcrumb homeHref="/admin/home" />
       <BadgePageTitle title="Meu Perfil" />
-      <Container />
+      <ContainerProfile />
     </div>
   );
 }
